441ec377bcfa7d6da987c494c0db499c74e001f3
0xTeaTwo <0xteatwo@protonmail.com>
2021-06-11 14:03:36 +0900

    refactor

--- src/utils/prices.test.ts
+++ src/utils/prices.test.ts
@@ -1,59 +1,74 @@
 import { formatPrice } from './prices';
 
 describe('formatPrice', () => {
-  it('works for eth', () => {
-    const oneWei = 0.000000000000000001;
-    expect(formatPrice(oneWei, 'eth').value).toBe('0.000000000');
-    const interger = 1;
-    expect(formatPrice(interger, 'eth').value).toBe('1');
-    const floored = 0.0000000019;
-    expect(formatPrice(floored, 'eth').value).toBe('0.000000001');
-    const eliminatedUnnecessaryZero = 0.0001;
-    expect(formatPrice(eliminatedUnnecessaryZero, 'eth').value).toBe('0.0001');
-    expect(formatPrice(eliminatedUnnecessaryZero, 'eth').value).not.toBe(
-      '0.000100000'
-    );
-    const comma = 12345.6789;
-    expect(formatPrice(comma, 'eth').value).toBe('12,345.6789');
+  describe('eth which is 9 digits', () => {
+    it('works for min value', () => {
+      const oneWei = 0.000000000000000001;
+      expect(formatPrice(oneWei, 'eth').value).toBe('0.000000000');
+    });
 
-    const nonValue = null;
-    expect(formatPrice(nonValue as any, 'eth').value).toBe('0');
-    const reallyZero = 0;
-    expect(formatPrice(reallyZero, 'eth').value).toBe('0');
+    it('works for interger', () => {
+      const interger = 1;
+      expect(formatPrice(interger, 'eth').value).toBe('1');
+      expect(formatPrice(interger, 'eth').value).not.toBe('1.000000000');
+    });
+
+    it('works to be floored if overflow', () => {
+      const floored = 0.0000000019;
+      expect(formatPrice(floored, 'eth').value).toBe('0.000000001');
+    });
+
+    it('works that Unnecessary Zero is eliminated', () => {
+      const eliminatedUnnecessaryZero = 0.0001;
+      expect(formatPrice(eliminatedUnnecessaryZero, 'eth').value).toBe(
+        '0.0001'
+      );
+      expect(formatPrice(eliminatedUnnecessaryZero, 'eth').value).not.toBe(
+        '0.000100000'
+      );
+    });
+
+    it('works for comma', () => {
+      const comma = 12345.6789;
+      expect(formatPrice(comma, 'eth').value).toBe('12,345.6789');
+    });
+
+    it('works that falsy value is 0', () => {
+      const nonValue = null;
+      expect(formatPrice(nonValue as any, 'eth').value).toBe('0');
+      const reallyZero = 0;
+      expect(formatPrice(reallyZero, 'eth').value).toBe('0');
+    });
   });
 
-  it('works for txjp', () => {
-    const minValue = 0.00000001;
-    expect(formatPrice(minValue, 'txjp').value).toBe('0.00000001');
-    const overflowed = 0.000000001;
-    expect(formatPrice(overflowed, 'txjp').value).toBe('0.00000000');
-    const floored = 0.000000019;
-    expect(formatPrice(floored, 'txjp').value).toBe('0.00000001');
-    const eliminatedUnnecessaryZero = 0.0001;
-    expect(formatPrice(eliminatedUnnecessaryZero, 'txjp').value).toBe('0.0001');
-    expect(formatPrice(eliminatedUnnecessaryZero, 'txjp').value).not.toBe(
-      '0.00010000'
-    );
-    const comma = 12345.6789;
-    expect(formatPrice(comma, 'txjp').value).toBe('12,345.6789');
+  describe('txjp which is 8 digits', () => {
+    it('works for min value', () => {
+      const minValue = 0.00000001;
+      expect(formatPrice(minValue, 'txjp').value).toBe('0.00000001');
+    });
 
-    const nonValue = null;
-    expect(formatPrice(nonValue as any, 'txjp').value).toBe('0');
-    const reallyZero = 0;
-    expect(formatPrice(reallyZero, 'txjp').value).toBe('0');
+    it('works to be floored if overflow', () => {
+      const floored = 0.000000019;
+      expect(formatPrice(floored, 'txjp').value).toBe('0.00000001');
+    });
   });
 
-  it('works for isZeroByRound', () => {
-    const oneWei = 0.000000000000000001;
-    expect(formatPrice(oneWei, 'eth').isZeroByRound).toBe(true);
-    const interger = 1;
-    expect(formatPrice(interger, 'eth').isZeroByRound).toBe(false);
-    const fractional = 0.1;
-    expect(formatPrice(fractional, 'eth').isZeroByRound).toBe(false);
+  describe('isZeroByRound', () => {
+    it('should be true', () => {
+      const oneWei = 0.000000000000000001; // -> `0.000000000`
+      expect(formatPrice(oneWei, 'eth').isZeroByRound).toBe(true);
+    });
 
-    const nonValue = null;
-    expect(formatPrice(nonValue as any, 'eth').isZeroByRound).toBe(false);
-    const reallyZero = 0;
-    expect(formatPrice(reallyZero, 'eth').isZeroByRound).toBe(false);
+    it('should be false', () => {
+      const interger = 1; // -> `1.000000000`
+      expect(formatPrice(interger, 'eth').isZeroByRound).toBe(false);
+      const fractional = 0.1;
+      expect(formatPrice(fractional, 'eth').isZeroByRound).toBe(false);
+
+      const nonValue = null;
+      expect(formatPrice(nonValue as any, 'eth').isZeroByRound).toBe(false);
+      const reallyZero = 0;
+      expect(formatPrice(reallyZero, 'eth').isZeroByRound).toBe(false);
+    });
   });
 });
